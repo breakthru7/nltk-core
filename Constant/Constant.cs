93d2457bd52ff8b0fae2fb79b0af23339fe57203
Eric Chong <eric.chong@cheetahdigital.com>
2020-02-03 16:53:47 +0800

    first commit

---
+++ Constant/Constant.cs
@@ -0,0 +1,10 @@
+using System; 
+
+namespace nltk_core
+{
+    public class Constants
+    {
+        //public static readonly string url_filter = $"{"http://nltk.default.svc.cluster.local"}/filter";
+        public static readonly string url_filter = "http://35.238.105.53/filter"; 
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text; 
+using Microsoft.AspNetCore.Mvc;
+using nltk_core;
+using nltk_core.Models;
+using Newtonsoft.Json;
+
+namespace nltk_core.Controllers
+{
+    public class HomeController : Controller
+    {
+        private HttpClient _client {get;set;} 
+
+        public HomeController ()
+        {
+            _client = new HttpClient(); 
+        }
+
+        public IActionResult Index()
+        {
+            var json = JsonConvert.SerializeObject( new { word_list = "This is a sentence with model XGA"}); 
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+ 
+            var response =  _client.PostAsync(Constants.url_filter, data).Result;
+
+            ViewData["msg"] = response.Content.ReadAsStringAsync().Result;
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your about page";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
